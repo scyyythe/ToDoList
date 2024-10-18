27a385a775d742fd433e19fd576fc8f3908a5157
scyyythe <caneteangel187@gmail.com>
2024-10-18 21:11:05 +0800

    dtbs

--- database/todo_db.sql
+++ database/todo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 02:57 AM
+-- Generation Time: Oct 18, 2024 at 03:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,49 @@ INSERT INTO `accounts` (`u_id`, `u_name`, `email`, `username`, `password`, `u_ty
 (43, 'Jamaica Anuba', 'jamaicaanuba@gmail.com', 'jai', '$2y$10$o5owZ8y3h07Vu23SW467tuxkVr14Z9jT7xLA4MTB0CXrEq2mXGC.O', 'User', 'Basic', 'Pending'),
 (44, 'Kyle Canete', 'lorencanete@yahoo.com', 'kyle', '$2y$10$GQ8XK9hmlL696.ZDrWZo0.KVddTDZQm8.DUK9tomqeC7bN/1/x5q2', 'User', 'Basic', 'Pending');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `folder_tbl`
+--
+
+CREATE TABLE `folder_tbl` (
+  `folder_id` int(100) NOT NULL,
+  `u_id` int(100) NOT NULL,
+  `folder_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `folder_tbl`
+--
+
+INSERT INTO `folder_tbl` (`folder_id`, `u_id`, `folder_name`) VALUES
+(18, 43, 'SIA'),
+(19, 43, 'multimedia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `note`
+--
+
+CREATE TABLE `note` (
+  `note_id` int(100) NOT NULL,
+  `u_id` int(100) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `note` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `due_date` date NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `note`
+--
+
+INSERT INTO `note` (`note_id`, `u_id`, `title`, `note`, `image`, `due_date`, `status`) VALUES
+(21, 43, 'ASDASD', 'ASDADS', '', '0000-00-00', 'Completed');
+
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +102,20 @@ ALTER TABLE `accounts`
   ADD PRIMARY KEY (`u_id`);
 
 --
+-- Indexes for table `folder_tbl`
+--
+ALTER TABLE `folder_tbl`
+  ADD PRIMARY KEY (`folder_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
+-- Indexes for table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`note_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,7 +123,35 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `u_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `u_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `folder_tbl`
+--
+ALTER TABLE `folder_tbl`
+  MODIFY `folder_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `note`
+--
+ALTER TABLE `note`
+  MODIFY `note_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `folder_tbl`
+--
+ALTER TABLE `folder_tbl`
+  ADD CONSTRAINT `folder_tbl_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `accounts` (`u_id`);
+
+--
+-- Constraints for table `note`
+--
+ALTER TABLE `note`
+  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `accounts` (`u_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
