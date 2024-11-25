be81eeef898ab79f9f1e71f7523afb868b69272e
scyyythe <caneteangel187@gmail.com>
2024-11-25 16:25:31 +0800

    database

--- database/todo_db (4).sql
+++ database/todo_db (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2024 at 04:17 PM
+-- Generation Time: Nov 25, 2024 at 09:25 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,7 +34,7 @@ CREATE TABLE `accounts` (
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `u_type` varchar(50) NOT NULL,
-  `plan` varchar(50) NOT NULL,
+  `plan_id` int(50) NOT NULL,
   `u_status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -42,9 +42,9 @@ CREATE TABLE `accounts` (
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`u_id`, `u_name`, `email`, `username`, `password`, `u_type`, `plan`, `u_status`) VALUES
-(41, 'Angel Canetee', 'caneteangel@yahoo.com', 'angel', '$2y$10$k8Eg6ezWxJbG2uiIC/MDs.ugP6oB5AF45NRvAk51LHmAkBkP8VZCq', 'Admin', 'Basic', 'Activee'),
-(54, 'Jamaica Anuba', 'caneteange@mgial.com', 'jai', '$2y$10$qj/16n6dVcWJqwsBoc6zpOgi83zvJMeDdln8IgHqCTf/RXzzhIBBm', 'User', 'Premuim', 'active');
+INSERT INTO `accounts` (`u_id`, `u_name`, `email`, `username`, `password`, `u_type`, `plan_id`, `u_status`) VALUES
+(69, 'Jamaica Anuba', 'jamaicaanuba@gmail.com', 'jai', '$2y$10$bPSZ8VHbqBPtdam0lSO4sOjG0kpbS63Q1joEZNb9DFp/o1jy/mhpi', 'User', 1001, 'Pending'),
+(70, 'Jeralyn Peritos', 'jeralynpreitos@gmail.com', 'jera', '$2y$10$EwtoSLSQzFpb7AJyRUyTn.ZxRgNhb0aR13vjKBIkFbYR3pWjeTCEy', 'User', 1002, 'Pending');
 
 -- --------------------------------------------------------
 
@@ -71,9 +71,16 @@ CREATE TABLE `note` (
   `note` text NOT NULL,
   `folder_id` int(100) DEFAULT NULL,
   `image` varchar(255) NOT NULL,
-  `due_date` date NOT NULL,
+  `deadline` time NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `note`
+--
+
+INSERT INTO `note` (`note_id`, `u_id`, `title`, `note`, `folder_id`, `image`, `deadline`, `status`) VALUES
+(137, 69, 'SIA', 'This is a sia.', NULL, '', '04:20:00', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -83,10 +90,44 @@ CREATE TABLE `note` (
 
 CREATE TABLE `payment_tbl` (
   `p_id` int(100) NOT NULL,
-  `u_id` int(100) NOT NULL,
-  `amount` int(100) NOT NULL,
+  `subscription_id` int(100) NOT NULL,
   `date_payment` date NOT NULL,
   `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plan_tbl`
+--
+
+CREATE TABLE `plan_tbl` (
+  `plan_id` int(100) NOT NULL,
+  `plan_name` varchar(50) NOT NULL,
+  `price` decimal(65,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `plan_tbl`
+--
+
+INSERT INTO `plan_tbl` (`plan_id`, `plan_name`, `price`) VALUES
+(1001, 'Basic', 0),
+(1002, 'Premuim', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription_tbl`
+--
+
+CREATE TABLE `subscription_tbl` (
+  `subscription_id` int(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `plan_id` int(100) NOT NULL,
+  `u_id` int(100) NOT NULL,
+  `subscription_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -97,7 +138,8 @@ CREATE TABLE `payment_tbl` (
 -- Indexes for table `accounts`
 --
 ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`u_id`);
+  ADD PRIMARY KEY (`u_id`),
+  ADD KEY `plan_id` (`plan_id`);
 
 --
 -- Indexes for table `folder_tbl`
@@ -119,6 +161,20 @@ ALTER TABLE `note`
 --
 ALTER TABLE `payment_tbl`
   ADD PRIMARY KEY (`p_id`),
+  ADD KEY `subscription_id` (`subscription_id`);
+
+--
+-- Indexes for table `plan_tbl`
+--
+ALTER TABLE `plan_tbl`
+  ADD PRIMARY KEY (`plan_id`);
+
+--
+-- Indexes for table `subscription_tbl`
+--
+ALTER TABLE `subscription_tbl`
+  ADD PRIMARY KEY (`subscription_id`),
+  ADD KEY `plan_id` (`plan_id`),
   ADD KEY `u_id` (`u_id`);
 
 --
@@ -129,19 +185,19 @@ ALTER TABLE `payment_tbl`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `u_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `u_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `folder_tbl`
 --
 ALTER TABLE `folder_tbl`
-  MODIFY `folder_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `folder_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `note`
 --
 ALTER TABLE `note`
-  MODIFY `note_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `note_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
 
 --
 -- AUTO_INCREMENT for table `payment_tbl`
@@ -150,8 +206,26 @@ ALTER TABLE `payment_tbl`
   MODIFY `p_id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `plan_tbl`
+--
+ALTER TABLE `plan_tbl`
+  MODIFY `plan_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1005;
+
+--
+-- AUTO_INCREMENT for table `subscription_tbl`
+--
+ALTER TABLE `subscription_tbl`
+  MODIFY `subscription_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`plan_id`) REFERENCES `plan_tbl` (`plan_id`);
 
 --
 -- Constraints for table `folder_tbl`
@@ -170,7 +244,14 @@ ALTER TABLE `note`
 -- Constraints for table `payment_tbl`
 --
 ALTER TABLE `payment_tbl`
-  ADD CONSTRAINT `payment_tbl_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `accounts` (`u_id`);
+  ADD CONSTRAINT `payment_tbl_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription_tbl` (`subscription_id`);
+
+--
+-- Constraints for table `subscription_tbl`
+--
+ALTER TABLE `subscription_tbl`
+  ADD CONSTRAINT `subscription_tbl_ibfk_1` FOREIGN KEY (`plan_id`) REFERENCES `plan_tbl` (`plan_id`),
+  ADD CONSTRAINT `subscription_tbl_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `accounts` (`u_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
